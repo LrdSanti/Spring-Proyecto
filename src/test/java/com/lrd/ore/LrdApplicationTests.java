892845c2dc3762a6a82822a9897d541e4797d107
LrdSanti <hernandez.santiago.5367@eam.edu.co>
2023-02-17 11:41:27 -0500

    Project changes example

---
+++ src/test/java/com/lrd/ore/LrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrd.ore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
